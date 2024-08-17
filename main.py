790401d79e80c1e7737569631eec129538834cec
Ulrik <4339ulrik@gmail.com>
2024-08-17 18:33:36 +0200

    implemented ratelimiting

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_simple_rate_limiter import rate_limiter
 import uvicorn
 import account
 import jobs
 import payload
 
 app = FastAPI()
-
 
 origins = [
     "http://msfs.skywingtool.com",
@@ -25,26 +25,55 @@ app.add_middleware(
 )
 
 
+class CustomException(Exception):
+    def __init__(self, status_code: int, message: str):
+        self.status_code = status_code
+        self.message = message
+
+
+@app.exception_handler(CustomException)
+async def custom_exception_handler(request: Request, exc: CustomException):
+    content = {
+        "success": False, 
+        "error": {
+            "message": {
+                "Message": exc.message,
+                "Error": "No Error specified",
+                "Status": 0,
+            }
+        }
+    }
+    
+    return  {
+                "status_code": exc.status_code,
+                "content": content,
+            }
+
 @app.get("/")
+@rate_limiter(limit=3, seconds=60)
 async def home():
     return {"Hello": "World"}
 
 
 # USER ACCOUNTS #
 @app.post("/apiv1/createuser")
+@rate_limiter(limit=10, seconds=60)
 async def CreateUser(data: payload.CreateUserBase):
     return await account.CreateAccount(data)
 
 @app.post("/apiv1/login")
+@rate_limiter(limit=10, seconds=60)
 async def Login(data: payload.LoginAccountBase):
     return await account.LoginAccount(data)
 
 @app.post("/apiv1/VerifyJToken")
+@rate_limiter(limit=10, seconds=60)
 async def VerifyJToken(data: Request):
     return await account.VerifyJToken(data.headers.get('TokenMSFS'))
 
 # JOBS #
 @app.get("/apiv1/GetAllJobs")
+@rate_limiter(limit=2, seconds=60)
 async def GetAllJobs(req: Request):
     x = await ValidateSession(req.headers.get('TokenMSFS'))
     if x['Status'] == 1:
@@ -53,6 +82,7 @@ async def GetAllJobs(req: Request):
         return x
 
 @app.get("/apiv1/GetRandomJobOffers")
+@rate_limiter(limit=2, seconds=60)
 async def GetRandomJobOffers(req: Request):
     x = await ValidateSession(req.headers.get('TokenMSFS'))
     if x['Status'] == 1:
@@ -61,6 +91,7 @@ async def GetRandomJobOffers(req: Request):
         return x
 
 @app.post("/apiv1/AcceptJobOffers")
+@rate_limiter(limit=2, seconds=60)
 async def AcceptJobOffers(req: Request, data: payload.AcceptJobOffers):
     x = await ValidateSession(req.headers.get('TokenMSFS'))
     if x['Status'] == 1:
@@ -69,6 +100,7 @@ async def AcceptJobOffers(req: Request, data: payload.AcceptJobOffers):
         return x
 
 @app.post("/apiv1/ApplyForJobs")
+@rate_limiter(limit=2, seconds=60)
 async def ApplyForJobs(req: Request, data: payload.AcceptJobOffers):
     x = await ValidateSession(req.headers.get('TokenMSFS'))
     if x['Status'] == 1:
